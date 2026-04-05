3e0d6b6797c88f62aa92f71b240c05c4a0937ad4
M-Sufyan003 <muhammadg115511@gmail.com>
2026-04-05 10:07:24 +0500

    Signup Feature Fully Completed

--- src/main/java/com/taskmanagementsystem/backend/service/AuthServiceImpl.java
+++ src/main/java/com/taskmanagementsystem/backend/service/AuthServiceImpl.java
@@ -5,32 +5,33 @@ import com.taskmanagementsystem.backend.entity.Role;
 import com.taskmanagementsystem.backend.entity.User;
 import com.taskmanagementsystem.backend.repository.UserRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Service
-public class AuthServiceImpl implements AuthService{
+public class AuthServiceImpl implements AuthService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public AuthServiceImpl(UserRepository userRepository)
-    {
-        this.userRepository=userRepository;
+    public AuthServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
-    public void signup(SignupRequest request)
-    {
+    public void signup(SignupRequest request) {
         // Step 1: Check if email already exists
-        if(userRepository.existsByEmail(request.getEmail()))
-        {
+        if (userRepository.existsByEmail(request.getEmail())) {
             throw new RuntimeException("Email already exists");
-            
+
         }
         // Step 2: Create User object
         User user = new User();
         user.setName(request.getName());
         user.setEmail(request.getEmail());
 
-        // Password hashing will be added next step
-        user.setPassword(request.getPassword());
+        // Password hashing is being applied before saving
+        String hashedPassword = passwordEncoder.encode(request.getPassword());
+        user.setPassword(hashedPassword);
 
         // Step 3: Assign default role
         user.setRole(Role.USER);
